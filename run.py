942b09276edb6799c89e216dcfc82b2cdc2c08f8
tangjing1995 <418636517@qq.com>
2020-12-17 00:14:21 +0800

    add .ideaa1

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ subject = "Phase2.0自动化Smoke测试报告"
 
 if __name__ == '__main__':
 
-    pytest.main([ "-s",'TestCase',  "--config", "alpha-config", "--pytest_report", report_html, #"-m","addmeter",deletemeter
+    pytest.main([ "-s",'TestCase\\test_01_login.py',  "--config", "alpha-config", "--pytest_report", report_html, #"-m","addmeter",deletemeter
         "--pytest_title", "自动化测试报告",
         "--pytest_desc", "----智燃云SmartHippo--- ",
         "--pytest_fw", "1.0",
